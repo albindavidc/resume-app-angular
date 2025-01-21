226f6f9040639898c8a724a4d398543b4a1cc06e
Albin David C <albindavidc007@gmail.com>
2025-01-21 16:14:38 +0530

    created all basic files

---
+++ src/app/user/create-resume/create-resume.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-resume',
+  imports: [],
+  templateUrl: './create-resume.component.html',
+  styleUrl: './create-resume.component.css'
+})
+export class CreateResumeComponent {
+
+}
