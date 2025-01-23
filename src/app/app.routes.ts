e44f09c9ed651bb604e4835aaa994d567064f777
Albin David C <albindavidc007@gmail.com>
2025-01-23 07:41:16 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: 'home',
+    component: LayoutComponent,
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'userList',
+    component: UserListComponent,
+  },
+  {
+    path: 'createUser',
+    component: CreateUserComponent,
+  },
+  {
+    path: 'updateUser',
+    component: UpdateUserComponent,
+  },
+];
